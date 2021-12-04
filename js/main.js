89c75a22bc02439b503cc59f01daa494e9d202fd
Juan Rojo A <jjrojoa@hotmail.com>
2021-12-03 20:36:31 -0500

    Some minimal style changes. Coded fully funtional modals.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,44 @@
-const inputBtnElement = document.getElementById("input-btn");
-const tabBtnElement = document.getElementById("tab-btn");
-const deleteBtnElement = document.getElementById("delete-btn");
+//Main buttons
+const inputBtnEl = document.getElementById("input-btn");
+const saveTabBtnEl = document.getElementById("tab-btn");
+const deleteAllBtnEl = document.getElementById("delete-btn");
+//Inputs
 const inputTitleEl = document.getElementById("input-title-el");
 const inputLinkEl = document.getElementById("input-link-el");
+//Modal Error
+const modalError = document.getElementById("modal-error");
+const errorModalContainer = document.getElementById("modal-container-error");
+const closeModalBtnEl = document.getElementById("close-modal-btn");
+//Modal Delete
+const modalDeleteAll = document.getElementById("modal-delete");
+const deleteAllModalContainer = document.getElementById("modal-containet-delete");
+const cancelModalBtnEl = document.getElementById("cancel-modal-btn");
+const deleteAllModalBtnEl = document.getElementById("delete-modal-btn");
+//List element
 const listEl = document.getElementById("list-el");
+//Links array
 let copiedLinks = [];
-let linksFromLocalStorage = JSON.parse(localStorage.getItem("copiedLinks"));
-
-if (linksFromLocalStorage) {
-  copiedLinks = linksFromLocalStorage;
+//Checks if localStore has links store and if so renders them
+if (localStorage.getItem("copiedLinks")) {
+  copiedLinks = JSON.parse(localStorage.getItem("copiedLinks"));
   renderLinks(copiedLinks);
 }
-
+//If enter is pressed on the title input the link input gets focus
 inputTitleEl.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
-    inputBtnElement.click();
+    inputLinkEl.focus();
   }
 });
-
+//If enter is pressed on the link input the event listener of the save input btn is fired up
 inputLinkEl.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
-    inputBtnElement.click();
+    inputBtnEl.click();
   }
 });
-
+//Deletes a single specific link
 listEl.addEventListener("click", function (e) {
   const item = e.target;
-  if (item.classList[0] === "delete-spc-btn") {
+  if (item.classList[0] === "delete-link-btn") {
     let itemID = item.id[0];
     copiedLinks.splice(itemID, 1);
     localStorage.setItem("copiedLinks", JSON.stringify(copiedLinks));
@@ -35,10 +47,10 @@ listEl.addEventListener("click", function (e) {
   }
   renderLinks(copiedLinks);
 });
-
-inputBtnElement.addEventListener("click", function (e) {
+//Adds a link from user input
+inputBtnEl.addEventListener("click", function (e) {
   if (e.keyCode === 13) {
-    inputBtnElement.click();
+    inputBtnEl.click();
   }
   if (inputLinkEl.value != "" && inputTitleEl.value != "") {
     copiedLinks.push({ title: inputTitleEl.value, link: inputLinkEl.value });
@@ -47,30 +59,63 @@ inputBtnElement.addEventListener("click", function (e) {
     inputTitleEl.value = "";
     inputTitleEl.focus();
     renderLinks(copiedLinks);
+  } else {
+    errorModalContainer.classList.remove("slide-out");
+    errorModalContainer.classList.add("slide-in");
+    modalError.classList.remove("display-none");
   }
 });
-
-tabBtnElement.addEventListener("click", function () {
+//Adds a link from current page
+saveTabBtnEl.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     copiedLinks.push({ title: tabs[0].title, link: tabs[0].url });
     localStorage.setItem("copiedLinks", JSON.stringify(copiedLinks));
     renderLinks(copiedLinks);
   });
 });
-
-deleteBtnElement.addEventListener("click", function () {
+//Fires up a modal to confirm the deletion of all links
+deleteAllBtnEl.addEventListener("click", function () {
+  if(localStorage.getItem("copiedLinks")){
+    deleteAllModalContainer.classList.remove("slide-out");
+    deleteAllModalContainer.classList.add("slide-in");
+    modalDeleteAll.classList.remove("display-none");
+  }
+});
+//Deletes all the links stored
+deleteAllModalBtnEl.addEventListener("click", function () {
+  deleteAllModalContainer.classList.add("slide-out");
+  deleteAllModalContainer.classList.remove("slide-in");
+  setTimeout(() => {
+    modalDeleteAll.classList.add("display-none");
+  }, 500);
   localStorage.clear();
   copiedLinks = [];
   renderLinks(copiedLinks);
 });
-
+//Closes delete all modal
+cancelModalBtnEl.addEventListener("click", () => {
+  deleteAllModalContainer.classList.add("slide-out");
+  deleteAllModalContainer.classList.remove("slide-in");
+  setTimeout(() => {
+    modalDeleteAll.classList.add("display-none");
+  }, 500);
+});
+//Closes the error modal
+closeModalBtnEl.addEventListener("click", () => {
+  errorModalContainer.classList.remove("slide-in");
+  errorModalContainer.classList.add("slide-out");
+  setTimeout(() => {
+    modalError.classList.add("display-none");
+  }, 500);
+});
+//Renders the stored links
 function renderLinks(array) {
   let listItems = "";
   for (i = 0; i < array.length; i++) {
     listItems += `
       <li>
         <a href="${array[i].link}" target="_blank">${array[i].title}</a>
-        <button class="delete-spc-btn" id="${i}"><span class="material-icons">
+        <button class="delete-link-btn" id="${i}"><span class="material-icons">
         delete_outline
         </span></button>
       </li>`;
